ae10ac6756e56d2394e65384e8715188de0ac167
savipaljessel <savipal.jessel@gmail.com>
2022-03-21 15:33:09 -0600

    last minute bug fixes

--- driver.cpp
+++ driver.cpp
@@ -26,8 +26,8 @@ bool Driver::start(std::istream &in)
     if (!res)
     {
         // tree->print();
-        //    if no semantic errors, print annotated AST
-        //    otherwise, return in failure
+        //     if no semantic errors, print annotated AST
+        //     otherwise, return in failure
         if (semanticAnalyzer(tree) != EXIT_FAILURE)
         {
             tree->print();
