4a2b34e7883d504e0c5b8b8991e47be68cd17044
savipal.jessel <savipal.jessel@csx.cs.ucalgary.ca>
2022-04-10 23:38:37 -0600

    penultimate commit

--- FINAL_PROJECT_OUTPUT/gen.t13.s
+++ FINAL_PROJECT_OUTPUT/gen.t13.s
@@ -12,19 +12,12 @@ char_0: .space 2
 	li	$a1,2
 	syscall
 	la	$t9,char_0
-	lb	$t8,0($t9)
-	bne	$t8,'-',char_0_end
-	li	$v0,8
-	la	$a0,char_0
-	li	$a1,2
-	syscall
-	la	$t9,char_0
-	lb	$t8,0($t9)
-	bne	$t8,'1',char_0_end
+	lb	$t9,0($t9)
+	li	$t8,0
+	bne	$t9,$t8,char_0_end
 	li	$v0,-1
 	j	char_0_fin
 char_0_end:
-	lb	$t9,0($t9)
 	move	$v0,$t9
 char_0_fin:
 	sw	$v0,4($sp)
